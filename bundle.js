4d5918f03362dd57904f22af0372a21cf7b9e5da
jjuchoa <jbfdujunior@gmail.com>
2019-02-19 22:07:14 -0300

    Operador SPREAD

--- bundle.js
+++ bundle.js
@@ -1,57 +1,22 @@
 "use strict";
 
-//REST ...
-//Em Objetos
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
-/* 
-const usuario = {
-    nome: 'Junior',
-    idade: 35,
-    cidade: 'Fortaleza'
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+//SPREAD
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
+var usuario1 = {
+  nome: 'Junior',
+  idade: 35,
+  cidade: 'Fortaleza'
 };
 
-const { nome, ...resto } = usuario
+var usuario2 = _objectSpread({}, usuario1, {
+  nome: 'Luna'
+});
 
-console.log(nome);
-console.log(resto);
-*/
-//Em Vetores
-
-/*
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-*/
-//Parametros de funções
-//Antes
-function soma1(a, b) {
-  return a + b;
-}
-
-console.log(soma1(1, 3)); //Depois
-
-function soma2() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-}
-
-console.log(soma2(1, 3, 4));
-
-function soma3(a, b) {
-  for (var _len2 = arguments.length, params = new Array(_len2 > 2 ? _len2 - 2 : 0), _key2 = 2; _key2 < _len2; _key2++) {
-    params[_key2 - 2] = arguments[_key2];
-  }
-
-  return params;
-}
-
-console.log(soma3(1, 3, 4, 5, 6, 7));
+console.log(usuario2);
--- main.js
+++ main.js
@@ -1,46 +1,18 @@
-//REST ...
-//Em Objetos
-/* 
-const usuario = {
+//SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+const usuario1 = {
     nome: 'Junior',
     idade: 35,
-    cidade: 'Fortaleza'
+    cidade: 'Fortaleza',
 };
 
-const { nome, ...resto } = usuario
+const usuario2 = { ...usuario1, nome: 'Luna' };
 
-console.log(nome);
-console.log(resto);
-*/
-
-//Em Vetores
-/*
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-*/
-
-//Parametros de funções
-//Antes
-function soma1 (a, b) {
-    return a + b;
-}
-
-console.log(soma1(1, 3));
-
-//Depois
-function soma2 (...params) {
-    return params.reduce((total, next) => total + next);
-}
-
-console.log(soma2(1, 3, 4));
-
-function soma3 (a, b, ...params) {
-    return params;
-}
-
-console.log(soma3(1, 3, 4, 5, 6, 7));
+console.log(usuario2);
