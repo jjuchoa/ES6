332e814a249086bbc9c5dc787fa3be03ad95374a
JOÃO Batista Ferreira Dias Uchoa Junior D001314 <D001314@DREADS.BNB>
2019-02-21 15:42:59 -0300

    Import/Export

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-alert('Testando');
+import * as funcoes from './funcoes';
+import soma, { sub } from './funcoes';
+import somaFunction from './soma';
+
+console.log(funcoes);
+console.log(funcoes.sub(4, 2));
+console.log(funcoes.mult(3, 3));
+console.log(soma(1, 2));
+console.log(sub(4, 2));
+console.log(somaFunction(2, 2));
