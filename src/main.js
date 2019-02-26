2810995cd049624144c95c0a1e49f82ec01b629c
JOÃO Batista Ferreira Dias Uchoa Junior D001314 <D001314@DREADS.BNB>
2019-02-26 14:43:45 -0300

    Loading e error

--- src/main.js
+++ src/main.js
@@ -15,6 +15,19 @@ class App {
          this.formEl.onsubmit = event => this.addRepository(event);
      }
 
+     setLoading (loading = true){
+         if (loading === true){
+             let loadingEl = document.createElement('span');
+             loadingEl.appendChild(document.createTextNode('Carregando'));
+             loadingEl.setAttribute('id', 'loading');
+
+             this.formEl.appendChild(loadingEl);
+         }
+         else {
+             document.getElementById('loading').remove();
+         }
+     }
+
      async addRepository (event) {
          event.preventDefault();
 
@@ -23,20 +36,29 @@ class App {
          if(repoInput.length === 0)
             return;
 
-         const response = await api.get(`/repos/${repoInput}`);
-         
-         const {name, description, html_url, owner:{ avatar_url}} = response.data;
+            this.setLoading();
+        
+            try {
+                const response = await api.get(`/repos/${repoInput}`);
 
-         this.repositories.push({
-             name,
-             description,
-             avatar_url,
-             html_url,
-         });
+                const {name, description, html_url, owner:{ avatar_url}} = response.data;
 
-         this.inputEl.value = '';
+                this.repositories.push({
+                    name,
+                    description,
+                    avatar_url,
+                    html_url,
+                });
+                
+                this.inputEl.value = '';
+                
+                this.render();
 
-         this.render();
+            } catch (error) {
+                alert('O repositório não existe!');
+            }
+            
+            this.setLoading(false);
      }
 
      render() {
